71b3d9f6ab71647ccef1e6f46997c9c368746405
Sardor Muminov <sardor@insilicogen.com>
2016-07-13 10:24:31 +0900

    Update settings

--- maslahat/settings.py
+++ maslahat/settings.py
@@ -152,4 +152,3 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 FACEBOOK_APP_ID = os.getenv("FACEBOOK_APP_ID")
 FACEBOOK_APP_SECRET = os.getenv("FACEBOOK_APP_SECRET")
 FACEBOOK_GROUP_ID = os.getenv("FACEBOOK_GROUP_ID")
-STATHAT_ACCOUNT = 'smuminov@gmail.com'
